26a41fce4c8707b961d52933debd4d59187c3448
deven <deven@DESKTOP-7P9J61I>
2026-04-30 21:21:56 -0400

    Evaluations page cols done

--- AAPS.Application/DTO/EvalDTO.cs
+++ AAPS.Application/DTO/EvalDTO.cs
@@ -4,87 +4,90 @@ namespace AAPS.Application.DTO;
 
 public record EvalDTO
 {
-    [DisplayField("Id")]
+    [DisplayField("Id", browsable: false, IsReadOnly = true)]
     public int Id { get; set; }
 
-    [DisplayField("First Name")]
-    public string? StudentFirstName { get; set; }
-
-    [DisplayField("Last Name")]
+    [DisplayField("Student Last Name")]
     public string? StudentLastName { get; set; }
+
+    [DisplayField("Student First Name")]
+    public string? StudentFirstName { get; set; }
 
     [DisplayField("Student ID")]
     public string? StudentId { get; set; }
 
+    [DisplayField("District")]
+    public string? District { get; set; }
+
+    [DisplayField("Service Type")]
+    public string? ServiceType { get; set; }
+
+    [DisplayField("Language")]
+    public string? Language { get; set; }
+
     [DisplayField("Phone")]
     public string? Phone { get; set; }
 
-    [DisplayField("Email")]
+    [DisplayField("Email", browsable: false)]
     public string? Email { get; set; }
 
-    [DisplayField("Parent First")]
-    public string? ParentFirstName { get; set; }
-
-    [DisplayField("Parent Last")]
+    [DisplayField("Parent Last Name")]
     public string? ParentLastName { get; set; }
 
-    [DisplayField("Eval Received")]
+    [DisplayField("Parent First Name")]
+    public string? ParentFirstName { get; set; }
+
+    [DisplayField("Referral Received")]
     public DateTime? EvalReceivedDate { get; set; }
 
     [DisplayField("Provider ID", browsable: false)]
     public int? ProviderId { get; set; }
 
-    [DisplayField("Assigned Date")]
+    [DisplayField("Provider First Name", browsable: false)]
+    public string? ProviderFirstName { get; set; }
+
+    [DisplayField("Provider")]
+    public string? ProviderLastName { get; set; }
+
+    [DisplayField("Unassigned Provider")]
+    public bool IsUnassigned { get; set; }
+
+    [DisplayField("Assigned On")]
     public DateTime? AssignedDate { get; set; }
+
+    [DisplayField("Appointment")]
+    public DateTime? AppointmentDate { get; set; }
 
     [DisplayField("Report Received")]
     public DateTime? ReportReceivedDate { get; set; }
 
-    [DisplayField("Eval Date")]
+    [DisplayField("Evaluation Date")]
     public DateTime? EvalDate { get; set; }
 
     [DisplayField("Report Submitted")]
     public DateTime? ReportSubmittedDate { get; set; }
 
-    [DisplayField("District")]
-    public string? District { get; set; }
+    [DisplayField("Status")]
+    public string? Status { get; set; }
 
-    [DisplayField("Language")]
-    public string? Language { get; set; }
-
-    [DisplayField("Service Type")]
-    public string? ServiceType { get; set; }
-
-    [DisplayField("Contact")]
+    [DisplayField("DoE Contact")]
     public string? Contact { get; set; }
 
-    [DisplayField("Provider Paid Amt")]
+    [DisplayField("Provider Paid Amt", browsable: false)]
     public decimal? ProviderPaidAmount { get; set; }
 
-    [DisplayField("Billing Amount")]
+    [DisplayField("Billing Amount", browsable: false)]
     public decimal? BillingAmount { get; set; }
-
-    [DisplayField("Provider Paid Date")]
-    public DateTime? ProviderPaidDate { get; set; }
-
-    [DisplayField("Bill Paid Date")]
-    public DateTime? BillPaidDate { get; set; }
 
     [DisplayField("Billed Date")]
     public DateTime? BilledDate { get; set; }
 
-    [DisplayField("Memo")]
+    [DisplayField("Bill Paid Date")]
+    public DateTime? BillPaidDate { get; set; }
+
+    [DisplayField("Provider Paid Date")]
+    public DateTime? ProviderPaidDate { get; set; }
+
+    [DisplayField("Memo", browsable: false)]
     public string? Memo { get; set; }
-
-    [DisplayField("Appointment Date")]
-    public DateTime? AppointmentDate { get; set; }
-
-    [DisplayField("Status")]
-    public string? Status { get; set; }
-
-    [DisplayField("Provider First")]
-    public string? ProviderFirstName { get; set; }
-
-    [DisplayField("Provider Last")]
-    public string? ProviderLastName { get; set; }
 }
